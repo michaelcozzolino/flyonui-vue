e8751acf806e91bb65a56cb0343500a93986fdd8
M.Cozzolino <michaelcozzolino@outlook.it>
2025-01-11 14:07:10 +0100

    feat: add FoSelect docs

---
+++ docs/Forms/Select/DefaultSelectSize.vue
@@ -0,0 +1,39 @@
+<template>
+    <FoSelect v-model="selectedOption"
+              :label="{ text: 'Pick your favorite Movie' }"
+              :options="options"
+              size="extraSmall"
+    />
+
+    <FoSelect v-model="selectedOption"
+              :label="{ text: 'Pick your favorite Movie' }"
+              :options="options"
+              size="small"
+    />
+
+    <FoSelect v-model="selectedOption"
+              :label="{ text: 'Pick your favorite Movie' }"
+              :options="options"
+    />
+
+    <FoSelect v-model="selectedOption"
+              :label="{ text: 'Pick your favorite Movie' }"
+              :options="options"
+              size="large"
+    />
+</template>
+
+<script setup lang="ts">
+import FoSelect, { type Option } from '@/Components/Select/UI/FoSelect.vue';
+import { ref }                   from 'vue';
+
+const options = ref<Option[]>([
+    { id: 1, text: 'The Godfather' },
+    { id: 2, text: 'The Shawshank Redemption' },
+    { id: 3, text: 'Pulp Fiction' },
+    { id: 4, text: 'The Dark Knight' },
+    { id: 5, text: `Schindler's List` },
+]);
+
+const selectedOption = ref<Option | null>(null);
+</script>
