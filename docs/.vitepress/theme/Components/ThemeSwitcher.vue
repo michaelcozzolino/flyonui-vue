c4c8d1fd53f1c70d9488c8a4f17ddb5e648e9e3e
M.Cozzolino <michaelcozzolino@outlook.it>
2025-01-11 11:28:04 +0100

    feat: Implement ThemeSwitcher for docs ui

---
+++ docs/.vitepress/theme/Components/ThemeSwitcher.vue
@@ -0,0 +1,7 @@
+<template>
+    <SelectThemeController storage-key="vitepress-theme-appearance" />
+</template>
+
+<script setup lang="ts">
+import SelectThemeController from '../../../../src/components/ThemeController/UI/SelectThemeController.vue';
+</script>
--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
             },
             {
                 find:        /^.*\/VPSwitchAppearance\.vue$/,
-                replacement: resolve(__dirname, '../src/components/ThemeController/UI/SelectThemeController.vue'),
+                replacement: resolve(__dirname, '.vitepress/theme/Components/ThemeSwitcher.vue'),
             },
         ],
     },
