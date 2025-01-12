5647f070b4b761e02bc0a6a70980ca24ba793e84
M.Cozzolino <michaelcozzolino@outlook.it>
2025-01-12 18:59:23 +0100

    feat: Implement FoMenu docs

---
+++ docs/Navigations/Menu/DefaultMenu.vue
@@ -0,0 +1,24 @@
+<template>
+    <FoMenu :items="items" />
+</template>
+
+<script setup lang="ts">
+import type { MenuItem } from '@/Components/Menu/Types/Menu';
+import FoMenu            from '@/Components/Menu/UI/FoMenu.vue';
+import { ref }           from 'vue';
+
+const items = ref<MenuItem[]>([
+    {
+        text: { value: 'Home' },
+        to:   '#',
+    },
+    {
+        text: { value: 'Account' },
+        to:   '#',
+    },
+    {
+        text: { value: 'Notifications' },
+        to:   '#',
+    },
+]);
+</script>
---
+++ docs/Navigations/Menu/MenuDocs.vue
@@ -0,0 +1,46 @@
+<template>
+    <CodeSnippet v-if="section === 'default'"
+                 :code="DefaultMenuRaw"
+                 :component="DefaultMenu"
+    />
+
+    <CodeSnippet v-else-if="section === 'icon'"
+                 :code="IconMenuRaw"
+                 :component="IconMenu"
+    />
+
+    <CodeSnippet v-else-if="section === 'only-icon'"
+                 :code="OnlyIconMenuRaw"
+                 :component="OnlyIconMenu"
+    />
+
+    <CodeSnippet v-else-if="section === 'orientation'"
+                 :code="MenuOrientationRaw"
+                 :component="MenuOrientation"
+    />
+
+    <CodeSnippet v-else-if="section === 'with-tooltip'"
+                 :code="MenuWithTooltipRaw"
+                 :component="MenuWithTooltip"
+    />
+</template>
+
+<script setup lang="ts">
+import CodeSnippet        from '../../Components/Button/CodeSnippet.vue';
+import DefaultMenu        from './DefaultMenu.vue';
+import DefaultMenuRaw     from './DefaultMenu.vue?raw';
+import IconMenu           from './IconMenu.vue';
+import IconMenuRaw        from './IconMenu.vue?raw';
+import MenuOrientation    from './MenuOrientation.vue';
+import MenuOrientationRaw from './MenuOrientation.vue?raw';
+import MenuWithTooltip    from './MenuWithTooltip.vue';
+import MenuWithTooltipRaw from './MenuWithTooltip.vue?raw';
+import OnlyIconMenu       from './OnlyIconMenu.vue';
+import OnlyIconMenuRaw    from './OnlyIconMenu.vue?raw';
+
+interface Props {
+    section: 'default' | 'icon' | 'only-icon' | 'orientation' | 'with-tooltip';
+}
+
+defineProps<Props>();
+</script>
