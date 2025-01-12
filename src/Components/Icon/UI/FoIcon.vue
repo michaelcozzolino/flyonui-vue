f8ab79afb12594f1d26bb3d76a8c8d6cffbc59dc
M.Cozzolino <michaelcozzolino@outlook.it>
2025-01-12 19:06:04 +0100

    feat: Implement FoIcon component

---
+++ src/Components/Icon/UI/FoIcon.vue
@@ -0,0 +1,27 @@
+<template>
+    <component :is="icon"
+               v-if="isIconTypeComponent(icon)"
+    />
+
+    <Icon v-else
+          :icon="icon"
+    />
+</template>
+
+<script setup lang="ts">
+import type { IconType }  from '@/Components/Icon/Types/Icon';
+import type { Component } from 'vue';
+import { Icon }           from '@iconify/vue';
+
+interface Props {
+    icon: IconType;
+}
+
+defineProps<Props>();
+
+function isIconTypeComponent(icon?: IconType): icon is Component {
+    return icon !== undefined
+        && typeof icon !== 'string'
+        && !('body' in icon); // The last condition checks whether the icon is IconifyIcon
+}
+</script>
