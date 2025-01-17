ee34f89305792e6fb04a96bd9fe5ea4ccf106d35
M.Cozzolino <michaelcozzolino@outlook.it>
2025-01-17 23:19:03 +0100

    feat: Move config files to packages

--- Packages/Core/tailwind.config.mjs
+++ Packages/Core/tailwind.config.mjs
@@ -2,8 +2,6 @@ import flyonui from 'flyonui';
 
 export default {
     content: [
-        './docs/.vitepress/**/*.{js,ts,vue}',
-        './docs/**/*.{js,ts,vue,md}',
         './src/**/*.{js,ts,vue}',
     ],
     plugins: [
