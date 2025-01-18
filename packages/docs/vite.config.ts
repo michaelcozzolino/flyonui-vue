952aaeaa3083e2614f8592d667060c2d6d65733c
M.Cozzolino <michaelcozzolino@outlook.it>
2025-01-19 00:13:10 +0100

    chore: update vite and remove deprecations

--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -10,4 +10,11 @@ export default defineConfig({
             },
         ],
     },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler',
+            },
+        },
+    },
 });
