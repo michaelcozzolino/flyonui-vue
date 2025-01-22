a6d4a94df62053b1ffba4c80849a1fea90cb7860
M.Cozzolino <michaelcozzolino@outlook.it>
2025-01-22 22:11:21 +0100

    feat(docs): Add Navbar docs

---
+++ packages/docs/Navigations/Navbar/DefaultNavbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <FoNavbar :links="links">
+        <template #start>
+            <FoNavbarBrand>
+                FlyonUI-vue
+            </FoNavbarBrand>
+        </template>
+    </FoNavbar>
+</template>
+
+<script setup lang="ts">
+import type { NavbarLink }         from 'flyonui-vue';
+import { FoNavbar, FoNavbarBrand } from 'flyonui-vue';
+import { computed }                from 'vue';
+
+const links = computed((): NavbarLink[] => {
+    return [
+        { id: 1, to: '/home', text: 'Home' },
+        { id: 2, to: '/service', text: 'Services' },
+        { id: 3, to: '/contact-us', text: 'Contact us' },
+    ];
+});
+</script>
---
+++ packages/docs/Navigations/Navbar/NavbarDocs.vue
@@ -0,0 +1,25 @@
+<template>
+    <CodeSnippet v-if="section === 'default'"
+                 :code="DefaultNavbarRaw"
+                 :component="DefaultNavbar"
+    />
+
+    <CodeSnippet v-else-if="section === 'with-logo'"
+                 :code="NavbarWithLogoRaw"
+                 :component="NavbarWithLogo"
+    />
+</template>
+
+<script setup lang="ts">
+import CodeSnippet       from '../../.vitepress/theme/Components/CodeSnippet.vue';
+import DefaultNavbar     from './DefaultNavbar.vue';
+import DefaultNavbarRaw  from './DefaultNavbar.vue?raw';
+import NavbarWithLogo    from './NavbarWithLogo.vue';
+import NavbarWithLogoRaw from './NavbarWithLogo.vue?raw';
+
+interface Props {
+    section: 'default' | 'with-logo';
+}
+
+defineProps<Props>();
+</script>
