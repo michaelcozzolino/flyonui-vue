4e10b654bd2b80dfb6787e3dea5b3a76580b7ad9
M.Cozzolino <michaelcozzolino@outlook.it>
2025-01-27 21:13:16 +0100

    feat: Add linktype to navbar to distinguish between vue and vanilla js

--- packages/docs/.vitepress/theme/Components/Navbar.vue
+++ packages/docs/.vitepress/theme/Components/Navbar.vue
@@ -1,5 +1,7 @@
 <template>
-    <FoNavbar>
+    <FoNavbar :links="links"
+              link-type="js"
+    >
         <template #start>
             <FoNavbarBrand>
                 FlyonUI-Vue
@@ -7,22 +9,33 @@
         </template>
 
         <template #end>
-            <div class="flex gap-2">
-                <FoSelectThemeController class="w-48" />
-                <FoSocialButton class="place-self-center"
-                                social="Github"
-                                shape="square"
-                                @click="goToGitHub"
-                />
-            </div>
+            <FoSelectThemeController class="!w-48" />
+
+            <FoSocialButton class="place-self-center"
+                            social="Github"
+                            shape="square"
+                            @click="goToGitHub"
+            />
         </template>
     </FoNavbar>
 </template>
 
 <script setup lang="ts">
-import { FoNavbar, FoNavbarBrand, FoSelectThemeController, FoSocialButton } from 'flyonui-vue';
+import { FoNavbar, FoNavbarBrand, FoSelectThemeController, FoSocialButton, type NavbarLink } from 'flyonui-vue';
+import { withBase }                                                                          from 'vitepress';
+import { computed }                                                                          from 'vue';
 
 function goToGitHub(): void {
     window.open('https://github.com/michaelcozzolino/flyonui-vue', '_blank', 'noopener,noreferrer');
 }
+
+const links = computed((): NavbarLink[] => {
+    return [
+        {
+            id:   1,
+            text: 'Playground',
+            to:   withBase('/playground/'),
+        },
+    ];
+});
 </script>
