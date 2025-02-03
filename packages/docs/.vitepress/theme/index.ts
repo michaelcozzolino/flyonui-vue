f2a709744c3f7a27909a2a62d8e081b8b55061dc
M.Cozzolino <michaelcozzolino@outlook.it>
2025-02-03 23:36:14 +0100

    feat: export styles from package.json and use them in docs

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -4,6 +4,7 @@ import { FoSelectThemeController }              from 'flyonui-vue';
 import DefaultTheme                             from 'vitepress/theme';
 import BadgeDocs                                from '../../Components/Badge/BadgeDocs.vue';
 import ButtonDocs                               from '../../Components/Button/ButtonDocs.vue';
+import ListGroupDocs                            from '../../Components/ListGroup/ListGroupDocs.vue';
 import StatListDocs                             from '../../Components/StatList/StatListDocs.vue';
 import CheckboxDocs                             from '../../Forms/Checkbox/CheckboxDocs.vue';
 import SelectDocs                               from '../../Forms/Select/SelectDocs.vue';
@@ -12,48 +13,23 @@ import NavbarDocs                               from '../../Navigations/Navbar/N
 import Playground                               from '../../Playground/Playground.vue';
 import './tailwind.css';
 import './style.scss';
+import 'flyonui-vue/index.css';
 
 // todo: imports could use aliases
 export default {
     extends: DefaultTheme,
     enhanceApp({ app }) {
         registerDocComponents(app, [
-            {
-                name:     'BadgeDocs',
-                instance: BadgeDocs,
-            },
-            {
-                name:     'ButtonDocs',
-                instance: ButtonDocs,
-            },
-            {
-                name:     'CheckboxDocs',
-                instance: CheckboxDocs,
-            },
-            {
-                name:     'MenuDocs',
-                instance: MenuDocs,
-            },
-            {
-                name:     'StatListDocs',
-                instance: StatListDocs,
-            },
-            {
-                name:     'SelectDocs',
-                instance: SelectDocs,
-            },
-            {
-                name:     'NavbarDocs',
-                instance: NavbarDocs,
-            },
-            {
-                name:     'SelectThemeController',
-                instance: FoSelectThemeController,
-            },
-            {
-                name:     'Playground',
-                instance: Playground,
-            },
+            { name: 'BadgeDocs', instance: BadgeDocs },
+            { name: 'ButtonDocs', instance: ButtonDocs },
+            { name: 'CheckboxDocs', instance: CheckboxDocs },
+            { name: 'ListGroupDocs', instance: ListGroupDocs },
+            { name: 'MenuDocs', instance: MenuDocs },
+            { name: 'NavbarDocs', instance: NavbarDocs },
+            { name: 'StatListDocs', instance: StatListDocs },
+            { name: 'SelectDocs', instance: SelectDocs },
+            { name: 'SelectThemeController', instance: FoSelectThemeController },
+            { name: 'Playground', instance: Playground },
         ]);
     },
 } satisfies Theme;
