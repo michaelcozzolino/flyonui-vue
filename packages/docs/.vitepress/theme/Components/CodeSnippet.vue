1cd119911893db070b9b2eb9213fd6cbbc910118
M.Cozzolino <michaelcozzolino@outlook.it>
2025-03-29 23:24:09 +0100

    chore: Upgrade tailwind in docs

--- packages/docs/.vitepress/theme/Components/CodeSnippet.vue
+++ packages/docs/.vitepress/theme/Components/CodeSnippet.vue
@@ -62,6 +62,7 @@ const previewGridClass = computed(() => {
             2: 'grid-cols-2',
             3: 'grid-cols-3',
             4: 'grid-cols-4',
+            5: 'grid-cols-5',
             7: 'grid-cols-7',
             8: 'grid-cols-8',
         },
@@ -70,6 +71,7 @@ const previewGridClass = computed(() => {
             2: 'grid-rows-2',
             3: 'grid-rows-3',
             4: 'grid-rows-4',
+            5: 'grid-rows-5',
             6: 'grid-rows-6',
         },
     };
