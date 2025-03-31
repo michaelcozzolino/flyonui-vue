c7c37767b4f9ce7cab3ed8efe26f5d761cba37da
M.Cozzolino <michaelcozzolino@outlook.it>
2025-03-31 23:52:14 +0200

    feat: Add Dashed badge and Button docs

---
+++ packages/docs/Components/Badge/DashedBadge.vue
@@ -0,0 +1,51 @@
+<template>
+    <FoBadge preset="dash">
+        Default
+    </FoBadge>
+
+    <FoBadge color="primary"
+             preset="dash"
+    >
+        Primary
+    </FoBadge>
+
+    <FoBadge color="secondary"
+             preset="dash"
+    >
+        Secondary
+    </FoBadge>
+
+    <FoBadge color="accent"
+             preset="dash"
+    >
+        Accent
+    </FoBadge>
+
+    <FoBadge color="info"
+             preset="dash"
+    >
+        Info
+    </FoBadge>
+
+    <FoBadge color="success"
+             preset="dash"
+    >
+        Success
+    </FoBadge>
+
+    <FoBadge color="warning"
+             preset="dash"
+    >
+        Warning
+    </FoBadge>
+
+    <FoBadge color="error"
+             preset="dash"
+    >
+        Error
+    </FoBadge>
+</template>
+
+<script setup lang="ts">
+import { FoBadge } from 'flyonui-vue';
+</script>
