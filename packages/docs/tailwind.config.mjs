8987910bc4d25611e0c05d79dd5f4175831356db
M.Cozzolino <michaelcozzolino@outlook.it>
2025-04-18 21:44:59 +0200

    feat: Add docs to install version 2.x

--- packages/docs/tailwind.config.mjs
+++
@@ -1,9 +0,0 @@
-import path from 'node:path';
-
-export default {
-    content: [
-        './.vitepress/**/*.{html,js,ts,vue}',
-        './{Components,Forms,Navigations}/**/*.{html,js,ts,vue}',
-        `${path.dirname(require.resolve('flyonui-vue'))}/**/*.{html,js,ts,vue}`,
-    ],
-};
