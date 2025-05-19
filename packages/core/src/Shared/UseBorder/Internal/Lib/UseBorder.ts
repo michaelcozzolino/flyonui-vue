52691eca781933245460149f8c02478c27cbeea9
M.Cozzolino <michaelcozzolino@outlook.it>
2025-05-19 22:44:26 +0200

    feat: Move UseBorder to a separate folder

---
+++ packages/core/src/Shared/UseBorder/Internal/Lib/UseBorder.ts
@@ -0,0 +1,15 @@
+import type { BorderableComponentName }       from '@/Shared/UseBorder';
+import type { ComputedRef, MaybeRefOrGetter } from 'vue';
+import { useClass }                           from '@/Shared/Internal';
+import { toValue }                            from 'vue';
+
+export function useBorder(
+    componentName: MaybeRefOrGetter<BorderableComponentName>,
+    isBordered: MaybeRefOrGetter<boolean>,
+): ComputedRef<string> {
+    const availableClasses: Record<BorderableComponentName, string> = {
+        FoStats: 'stats-border shadow-none',
+    };
+
+    return useClass(isBordered, availableClasses[toValue(componentName)]);
+}
---
+++ packages/core/src/Shared/UseBorder/Types/Border.ts
@@ -0,0 +1,3 @@
+import type { ComponentName } from '@/Shared/Types/ComponentTypes.ts';
+
+export type BorderableComponentName = Extract<ComponentName, 'FoStats'>;
