38547659efc2f1f87552da74e8440912592f6846
M.Cozzolino <michaelcozzolino@outlook.it>
2025-05-24 13:11:59 +0200

    feat: Add missing exports and component names

--- packages/core/src/Shared/Utils/Internal/Types/ComponentName.ts
+++ packages/core/src/Shared/Utils/Internal/Types/ComponentName.ts
@@ -1,5 +1,17 @@
-import type { ConfigurableComponentProps } from '@/Configuration/CreateFlyonUIVueApp';
+import type { ConfigurableComponentProps } from '@/Shared/UseFlyonUIVueAppConfig';
 
-type NonConfigurableComponentName = 'FoLabel' | 'FoStat' | 'FoStatDescription' | 'FoStatTitle' | 'FoStatValue';
+type NonConfigurableComponentName = 'FoJoin'
+    | 'FoCheckboxGroup'
+    | 'FoLabel'
+    | 'FoListGroup'
+    | 'FoListGroupItem'
+    | 'FoMenuItem'
+    | 'FoStat'
+    | 'FoStatDescription'
+    | 'FoStats'
+    | 'FoStatTitle'
+    | 'FoStatValue'
+    | 'FoSwap';
+
 export type ConfigurableComponentName = keyof ConfigurableComponentProps;
 export type ComponentName = ConfigurableComponentName | NonConfigurableComponentName;
