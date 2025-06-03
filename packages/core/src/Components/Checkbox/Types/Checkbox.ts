f782d285a9f569596f597270b5ffdb00a8146268
M.Cozzolino <michaelcozzolino@outlook.it>
2025-06-03 22:27:37 +0200

    refactor: Rename IsValid to Validity

--- packages/core/src/Components/Checkbox/Types/Checkbox.ts
+++ packages/core/src/Components/Checkbox/Types/Checkbox.ts
@@ -3,9 +3,9 @@ import type { MaybeStringId } from '@/Shared/UseIdentifiable';
 import type { Orientable }    from '@/Shared/UseOrientation';
 import type { Sizable }       from '@/Shared/UseSize';
 import type { Disableable }   from '@/Shared/UseState';
-import type { IsValid }       from '@/Shared/UseValidity';
+import type { Validity }      from '@/Shared/UseValidity';
 
-export interface CheckboxProps extends MaybeStringId, Disableable, IsValid, Sizable {
+export interface CheckboxProps extends MaybeStringId, Disableable, Validity, Sizable {
     color?:      Color;
     label?:      string;
     helperText?: string;
