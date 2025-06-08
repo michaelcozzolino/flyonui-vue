fc010625aba8b501f853f506aa3c4085e9bbeea3
M.Cozzolino <michaelcozzolino@outlook.it>
2025-06-08 19:21:20 +0200

    fix: docs typo

--- packages/docs/Overlays/Tooltip/DefaultTooltip.vue
+++ packages/docs/Overlays/Tooltip/DefaultTooltip.vue
@@ -3,11 +3,7 @@
         <FoButton icon="tabler:chevron-up" />
 
         <template #body>
-            <span class="text-lg font-medium">Tooltip title</span>
-            <p class="text-base pt-4">
-                This text serves as placeholder content for the tooltip, showcasing its overall look in the user
-                interface.
-            </p>
+            Tooltip on top
         </template>
     </FoTooltip>
 </template>
