bff954d1455d69529fe783671aa7649bf86ad20d
M.Cozzolino <michaelcozzolino@outlook.it>
2025-07-20 23:50:25 +0200

    feat: Implement missing select docs

--- packages/docs/Forms/Select/SelectDocs.vue
+++ packages/docs/Forms/Select/SelectDocs.vue
@@ -24,22 +24,79 @@
                  :code="SelectFloatingLabelSizeRaw"
                  :component="SelectFloatingLabelSize"
     />
+
+    <CodeSnippet v-else-if="section === 'validation-state'"
+                 id="validation-state"
+                 :code="SelectValidationStateRaw"
+                 :component="SelectValidationState"
+    />
+
+    <CodeSnippet v-else-if="section === 'shape'"
+                 id="shape"
+                 :code="SelectShapeRaw"
+                 :component="SelectShape"
+    />
+
+    <CodeSnippet v-else-if="section === 'with-label-and-helper-text'"
+                 id="with-label-and-helper-text"
+                 :code="SelectWithLabelAndHelperTextRaw"
+                 :component="SelectWithLabelAndHelperText"
+    />
+
+    <CodeSnippet v-else-if="section === 'disabled'"
+                 id="disabled"
+                 :code="DisabledSelectRaw"
+                 :component="DisabledSelect"
+    />
+
+    <CodeSnippet v-else-if="section === 'datalist'"
+                 id="datalist"
+                 :code="SelectAsDatalistRaw"
+                 :component="SelectAsDatalist"
+    />
+
+    <CodeSnippet v-else-if="section === 'optgroup'"
+                 id="optgroup"
+                 :code="SelectWithOptgroupRaw"
+                 :component="SelectWithOptgroup"
+    />
 </template>
 
 <script setup lang="ts">
-import type { Default }           from 'flyonui-vue';
-import CodeSnippet                from '@/.vitepress/theme/Components/CodeSnippet/UI/CodeSnippet.vue';
-import DefaultSelect              from '@/Forms/Select/DefaultSelect.vue';
-import DefaultSelectRaw           from '@/Forms/Select/DefaultSelect.vue?raw';
-import DefaultSelectSize          from '@/Forms/Select/DefaultSelectSize.vue';
-import DefaultSelectSizeRaw       from '@/Forms/Select/DefaultSelectSize.vue?raw';
-import SelectFloatingLabel        from '@/Forms/Select/SelectFloatingLabel.vue';
-import SelectFloatingLabelRaw     from '@/Forms/Select/SelectFloatingLabel.vue?raw';
-import SelectFloatingLabelSize    from '@/Forms/Select/SelectFloatingLabelSize.vue';
-import SelectFloatingLabelSizeRaw from '@/Forms/Select/SelectFloatingLabelSize.vue?raw';
+import type { Default }                from 'flyonui-vue';
+import CodeSnippet                     from '@/.vitepress/theme/Components/CodeSnippet/UI/CodeSnippet.vue';
+import DefaultSelect                   from '@/Forms/Select/DefaultSelect.vue';
+import DefaultSelectRaw                from '@/Forms/Select/DefaultSelect.vue?raw';
+import DefaultSelectSize               from '@/Forms/Select/DefaultSelectSize.vue';
+import DefaultSelectSizeRaw            from '@/Forms/Select/DefaultSelectSize.vue?raw';
+import DisabledSelect                  from '@/Forms/Select/DisabledSelect.vue';
+import DisabledSelectRaw               from '@/Forms/Select/DisabledSelect.vue?raw';
+import SelectAsDatalist                from '@/Forms/Select/SelectAsDatalist.vue';
+import SelectAsDatalistRaw             from '@/Forms/Select/SelectAsDatalist.vue?raw';
+import SelectFloatingLabel             from '@/Forms/Select/SelectFloatingLabel.vue';
+import SelectFloatingLabelRaw          from '@/Forms/Select/SelectFloatingLabel.vue?raw';
+import SelectFloatingLabelSize         from '@/Forms/Select/SelectFloatingLabelSize.vue';
+import SelectFloatingLabelSizeRaw      from '@/Forms/Select/SelectFloatingLabelSize.vue?raw';
+import SelectShape                     from '@/Forms/Select/SelectShape.vue';
+import SelectShapeRaw                  from '@/Forms/Select/SelectShape.vue?raw';
+import SelectValidationState           from '@/Forms/Select/SelectValidationState.vue';
+import SelectValidationStateRaw        from '@/Forms/Select/SelectValidationState.vue?raw';
+import SelectWithLabelAndHelperText    from '@/Forms/Select/SelectWithLabelAndHelperText.vue';
+import SelectWithLabelAndHelperTextRaw from '@/Forms/Select/SelectWithLabelAndHelperText.vue?raw';
+import SelectWithOptgroup              from '@/Forms/Select/SelectWithOptgroup.vue';
+import SelectWithOptgroupRaw           from '@/Forms/Select/SelectWithOptgroup.vue?raw';
 
 interface Props {
-    section: Default | 'floating-label' | 'default-size' | 'floating-label-size';
+    section: Default
+        | 'floating-label'
+        | 'default-size'
+        | 'floating-label-size'
+        | 'validation-state'
+        | 'shape'
+        | 'with-label-and-helper-text'
+        | 'disabled'
+        | 'datalist'
+        | 'optgroup';
 }
 
 defineProps<Props>();
