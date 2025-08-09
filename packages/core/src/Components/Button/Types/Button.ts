2215a4044e821550c97858d553976311cdce855c
M.Cozzolino <michaelcozzolino@outlook.it>
2025-08-09 14:37:33 +0200

    feat(docs): Annotate button props

--- packages/core/src/Components/Button/Types/Button.ts
+++ packages/core/src/Components/Button/Types/Button.ts
@@ -13,16 +13,29 @@ import type { RouteRecordRaw }       from 'vue-router';
 // todo: in case it is used by other components, just move it to shared
 export type Layout = Default | 'wide' | 'block';
 
-export interface ButtonProps extends MaybeStringId, Colorable, Disableable, Responsive, Shapeable, Sizable, WithGlass, WithConfigurableIcon {
-    to?:       RouteRecordRaw;
-    layout?:   Layout;
+export interface ButtonProps extends MaybeStringId,
+    Colorable,
+    Disableable,
+    Presettable,
+    Responsive,
+    Shapeable,
+    Sizable,
+    WithGlass,
+    WithConfigurableIcon {
+    /** Navigation target route, when given the button will act as a link */
+    to?: RouteRecordRaw;
+
+    /** The button's layout */
+    layout?: Layout;
+
+    /** The button's active state */
     isActive?: boolean;
 }
 
 export type Social = 'Facebook' | 'Twitter' | 'Linkedin' | 'Github';
 export type SocialButtonPreset = Extract<Preset, 'solid' | 'soft' | 'outline'>;
 
-export interface SocialButtonProps extends Omit<ButtonProps, 'preset'> {
-    social:  Social;
-    preset?: SocialButtonPreset;
+export interface SocialButtonProps extends Presettable<SocialButtonPreset> {
+    /** The button's social platform */
+    social: Social;
 }
