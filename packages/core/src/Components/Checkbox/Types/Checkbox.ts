81973bd1329006e2050a6581fe907b051654eeca
M.Cozzolino <michaelcozzolino@outlook.it>
2025-08-09 23:42:54 +0200

    feat(docs): Add more annotations

--- packages/core/src/Components/Checkbox/Types/Checkbox.ts
+++ packages/core/src/Components/Checkbox/Types/Checkbox.ts
@@ -1,14 +1,12 @@
-import type { Colorable }     from '@/Shared/UseColor';
-import type { MaybeStringId } from '@/Shared/UseIdentifiable';
-import type { Orientable }    from '@/Shared/UseOrientation';
-import type { Sizable }       from '@/Shared/UseSize';
-import type { Disableable }   from '@/Shared/UseState';
-import type { Validity }      from '@/Shared/UseValidity';
+import type { WithLabel }      from '@/Components';
+import type { WithHelperText } from '@/Components/HelperText';
+import type { Colorable }      from '@/Shared/UseColor';
+import type { MaybeStringId }  from '@/Shared/UseIdentifiable';
+import type { Orientable }     from '@/Shared/UseOrientation';
+import type { Sizable }        from '@/Shared/UseSize';
+import type { Disableable }    from '@/Shared/UseState';
+import type { Validity }       from '@/Shared/UseValidity';
 
-export interface CheckboxProps extends MaybeStringId, Disableable, Validity, Sizable {
-    color?:      Color;
-    label?:      string;
-    helperText?: string;
-}
+export type CheckboxProps = MaybeStringId & Colorable & Disableable & Sizable & Validity & WithHelperText & WithLabel;
 
 export type CheckboxGroupProps = Orientable;
--- packages/core/src/Components/InputText/Types/InputText.ts
+++ packages/core/src/Components/InputText/Types/InputText.ts
@@ -1,31 +1,27 @@
 import type { WithConfigurableHelperText } from '@/Components/HelperText';
 import type { WithConfigurableIcon }       from '@/Components/Icon';
-import type { InputLabel }                 from '@/Components/Label';
+import type { WithConfigurableInputLabel } from '@/Components/Label';
 import type { MaybeStringId }              from '@/Shared/UseIdentifiable';
 import type { Shape, Shapeable }           from '@/Shared/UseShape';
 import type { Sizable }                    from '@/Shared/UseSize';
 import type { Disableable }                from '@/Shared/UseState';
 import type { Validity }                   from '@/Shared/UseValidity';
-import type { HasPlaceholder, Immutable }  from '@/Shared/Utils/Types/Props.ts';
-
-export type InputTextShape = Extract<Shape, 'rounded' | 'pilled'>;
+import type { Immutable, WithPlaceholder } from '@/Shared/Utils/Types/Props.ts';
 
 // todo: the input label can be documented through a specific label docs even if no one will ever use the component FoLabel as it is internal
 
 export interface InputTextProps extends MaybeStringId,
     Disableable,
-    HasPlaceholder,
+    WithPlaceholder,
     Immutable,
-    Shapeable<InputTextShape>,
+    Shapeable<Extract<Shape, 'rounded' | 'pilled'>>,
     Sizable,
     Validity,
     WithConfigurableHelperText,
-    WithConfigurableIcon {
+    WithConfigurableIcon,
+    WithConfigurableInputLabel {
     /** The input's type */
     type?: 'text' | 'email';
-
-    /** The input's label text or an object containing "text" and "type" where type can be "text" | "floating" | "inline" */
-    label?: string | InputLabel;
 
     /** It does not show any animations when the input is focused */
     withoutFocus?: boolean;
