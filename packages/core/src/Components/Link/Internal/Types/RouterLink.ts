7131290f20792d1c5c4436294db0e2b0636950e7
M.Cozzolino <michaelcozzolino@outlook.it>
2025-08-10 12:02:39 +0200

    feat(docs): Add more annotations

--- packages/core/src/Components/Link/Internal/Types/RouterLink.ts
+++ packages/core/src/Components/Link/Internal/Types/RouterLink.ts
@@ -1,7 +1,7 @@
-import type { Navigation, To }  from '@/Components/Link';
-import type { RouterLinkProps } from 'vue-router';
+import type { Navigation, WithTo } from '@/Components/Link';
+import type { RouterLinkProps }    from 'vue-router';
 
-export interface FoRouterLinkProps extends RouterLinkProps {
+export interface FoRouterLinkProps extends Omit<RouterLinkProps, 'to'>, WithTo {
+    /** Custom navigation handler, to be used only in apps that do not support Vue Router E.G Vitepress */
     navigation?: Navigation;
-    to:          To;
 }
--- packages/core/src/Components/Link/Types/Link.ts
+++ packages/core/src/Components/Link/Types/Link.ts
@@ -2,17 +2,27 @@ import type { FoRouterLinkProps } from '@/Components/Link/Internal';
 import type { Colorable }         from '@/Shared/UseColor';
 import type { RouteLocationRaw }  from 'vue-router';
 
-export type To = string | RouteLocationRaw;
+export type Link = string;
+
+export type To = Link | RouteLocationRaw;
 
 export type UnderlineLinkEffect = 'hover' | 'hover-animated' | 'no-underline';
 
-export interface LinkProps extends FoRouterLinkProps {
-    color?:           Color;
+export interface LinkProps extends FoRouterLinkProps, Colorable {
+    /** If true, when the link is hovered it will be underlined */
     underlineEffect?: UnderlineLinkEffect;
+}
+
+export interface WithTo {
+    /** Target navigation url, if string, it is a standard url, else a Vue Router location */
+    to?: To;
 }
 
 // to be used only for SPA that do not use vue-router
 export interface Navigation {
-    navigate:   (to: string) => Promise<void>;
+    /** Navigate to a path */
+    navigate: (to: string) => Promise<void>;
+
+    /** Currently active path */
     activePath: string;
 }
