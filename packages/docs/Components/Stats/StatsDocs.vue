ba05479026a38fc4f92f372d3781b8b4832876a8
M.Cozzolino <michaelcozzolino@outlook.it>
2025-08-10 22:57:14 +0200

    feat(docs): Add props and slots api for FoStats.vue

--- packages/docs/Components/Stats/StatsDocs.vue
+++ packages/docs/Components/Stats/StatsDocs.vue
@@ -58,22 +58,22 @@ import type { ApiType }          from '@/Api/Types/Api.ts';
 import type { Default }          from 'flyonui-vue';
 import CodeSnippet               from '@/.vitepress/theme/Components/CodeSnippet/UI/CodeSnippet.vue';
 import ComponentsApiDocs         from '@/Api/UI/ComponentsApiDocs.vue';
-import BorderedStats             from '@/Components/Stat/BorderedStats.vue';
-import BorderedStatsRaw          from '@/Components/Stat/BorderedStats.vue?raw';
-import CenteredItemStats         from '@/Components/Stat/CenteredItemStats.vue';
-import CenteredItemStatsRaw      from '@/Components/Stat/CenteredItemStats.vue?raw';
-import DefaultStats              from '@/Components/Stat/DefaultStats.vue';
-import DefaultStatsRaw           from '@/Components/Stat/DefaultStats.vue?raw';
-import StatsWithActionsButton    from '@/Components/Stat/StatsWithActionsButton.vue';
-import StatsWithActionsButtonRaw from '@/Components/Stat/StatsWithActionsButton.vue?raw';
-import StatsWithAvatar           from '@/Components/Stat/StatsWithAvatar.vue';
-import StatsWithAvatarRaw        from '@/Components/Stat/StatsWithAvatar.vue?raw';
-import StatsWithIconsAndImage    from '@/Components/Stat/StatsWithIconsAndImage.vue';
-import StatsWithIconsAndImageRaw from '@/Components/Stat/StatsWithIconsAndImage.vue?raw';
-import StatsWithProgressBar      from '@/Components/Stat/StatsWithProgressBar.vue';
-import StatsWithProgressBarRaw   from '@/Components/Stat/StatsWithProgressBar.vue?raw';
-import VerticalStats             from '@/Components/Stat/VerticalStats.vue';
-import VerticalStatsRaw          from '@/Components/Stat/VerticalStats.vue?raw';
+import BorderedStats             from '@/Components/Stats/BorderedStats.vue';
+import BorderedStatsRaw          from '@/Components/Stats/BorderedStats.vue?raw';
+import CenteredItemStats         from '@/Components/Stats/CenteredItemStats.vue';
+import CenteredItemStatsRaw      from '@/Components/Stats/CenteredItemStats.vue?raw';
+import DefaultStats              from '@/Components/Stats/DefaultStats.vue';
+import DefaultStatsRaw           from '@/Components/Stats/DefaultStats.vue?raw';
+import StatsWithActionsButton    from '@/Components/Stats/StatsWithActionsButton.vue';
+import StatsWithActionsButtonRaw from '@/Components/Stats/StatsWithActionsButton.vue?raw';
+import StatsWithAvatar           from '@/Components/Stats/StatsWithAvatar.vue';
+import StatsWithAvatarRaw        from '@/Components/Stats/StatsWithAvatar.vue?raw';
+import StatsWithIconsAndImage    from '@/Components/Stats/StatsWithIconsAndImage.vue';
+import StatsWithIconsAndImageRaw from '@/Components/Stats/StatsWithIconsAndImage.vue?raw';
+import StatsWithProgressBar      from '@/Components/Stats/StatsWithProgressBar.vue';
+import StatsWithProgressBarRaw   from '@/Components/Stats/StatsWithProgressBar.vue?raw';
+import VerticalStats             from '@/Components/Stats/VerticalStats.vue';
+import VerticalStatsRaw          from '@/Components/Stats/VerticalStats.vue?raw';
 
 interface Props {
     section: Default
