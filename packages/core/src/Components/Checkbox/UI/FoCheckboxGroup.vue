bf2e2b45cd1f1887a712885156cc1516f3b06b10
M.Cozzolino <michaelcozzolino@outlook.it>
2025-08-10 22:38:41 +0200

    feat(docs): Add props and slots api for FoCheckbox.vue

--- packages/core/src/Components/Checkbox/UI/FoCheckboxGroup.vue
+++ packages/core/src/Components/Checkbox/UI/FoCheckboxGroup.vue
@@ -2,19 +2,23 @@
     <div class="flex"
          :class="orientationClass"
     >
-        <slot />
+        {{ useRequiredSlotMessage('FoCheckboxGroup') }}
     </div>
 </template>
 
 <script setup lang="ts">
 import type { CheckboxGroupProps }       from '@/Components/Checkbox';
+import type { WithRequiredDefaultSlot }  from '@/Shared/Utils/Types/Slots';
 import { isInCheckboxGroupInjectionKey } from '@/Components/Checkbox/Internal';
 import { useOrientation }                from '@/Shared/UseOrientation/Internal';
+import { useRequiredSlotMessage }        from '@/Shared/Utils/Internal';
 import { provide }                       from 'vue';
 
 const props = withDefaults(defineProps<CheckboxGroupProps>(), {
     orientation: 'horizontal',
 });
+
+defineSlots<WithRequiredDefaultSlot>();
 
 provide(isInCheckboxGroupInjectionKey, true);
 
