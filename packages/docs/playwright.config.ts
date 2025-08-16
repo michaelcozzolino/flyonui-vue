ee2153037f60b844ce2660fd45285678739d881f
M.Cozzolino <michaelcozzolino@outlook.it>
2025-08-16 20:40:54 +0200

    feat(docs): hide sidebars during browser tests, due avoid side effects on screenshots

--- packages/docs/playwright.config.ts
+++ packages/docs/playwright.config.ts
@@ -20,7 +20,6 @@ export default defineConfig({
             name: 'chromium',
             use:  {
                 ...devices['Desktop Chrome'],
-                viewport:      { width: 1280, height: 720 },
                 launchOptions: {
                     args: [
                         '--disable-font-subpixel-positioning',
