7d1dee236ec44a7c21dc3827aa739d195cddb74b
M.Cozzolino <michaelcozzolino@outlook.it>
2025-08-16 16:54:24 +0200

    feat(docs): Remove launch options for playwright.config.ts

--- packages/docs/playwright.config.ts
+++ packages/docs/playwright.config.ts
@@ -17,11 +17,6 @@ export default defineConfig({
             name: 'chromium',
             use:  {
                 ...devices['Desktop Chrome'],
-                launchOptions: {
-                    args: [
-                        '--disable-font-subpixel-positioning',
-                    ],
-                },
                 baseURL,
             },
         },
