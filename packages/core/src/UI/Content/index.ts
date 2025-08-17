75a1ac0def6b368db65541a18240c4b7aeb23245
M.Cozzolino <michaelcozzolino@outlook.it>
2025-08-17 17:13:25 +0200

    refactor(core): export components through new structure

---
+++ packages/core/src/UI/Content/index.ts
@@ -0,0 +1,4 @@
+export * from '@/UI/Content/Heading';
+export * from '@/UI/Content/Keyboard';
+export * from '@/UI/Content/Link';
+export * from '@/UI/Content/Mask';
---
+++ packages/core/src/UI/Forms/index.ts
@@ -0,0 +1,5 @@
+export * from '@/UI/Forms/Checkbox';
+export * from '@/UI/Forms/InputText';
+export * from '@/UI/Forms/Join';
+export * from '@/UI/Forms/Select';
+export * from '@/UI/Forms/Textarea';
