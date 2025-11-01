615337d18220deacd3e64cad14419124b7227010
Michael Cozzolino <michaelcozzolino@outlook.it>
2025-11-01 19:43:45 +0100

    Update version number from v2.1.0 to v2.1.1

--- packages/docs/.vitepress/theme/Components/Layout/Features/Navbar/UI/Navbar.vue
+++ packages/docs/.vitepress/theme/Components/Layout/Features/Navbar/UI/Navbar.vue
@@ -15,7 +15,7 @@
 
                     <!--                    todo: this should be fetched through npm -->
                     <p class="text-sm text-primary select-none">
-                        v2.1.0
+                        v2.1.1
                     </p>
 
                     <FoNavbarBrand :class="!isSidebarCollapsed && 'sm:ms-40'"
