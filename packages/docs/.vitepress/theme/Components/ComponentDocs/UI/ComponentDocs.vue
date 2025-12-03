463a90bfa1f201a3b5e3a9516537dfe7adaa0076
M.Cozzolino <michaelcozzolino@outlook.it>
2025-12-03 22:48:55 +0100

    refactor(docs): Adjust namespaces

--- packages/docs/.vitepress/theme/Components/ComponentDocs/UI/ComponentDocs.vue
+++ packages/docs/.vitepress/theme/Components/ComponentDocs/UI/ComponentDocs.vue
@@ -12,7 +12,7 @@
 </template>
 
 <script setup lang="ts" generic="S extends string, C extends ComponentDocsPreview">
-import type { ComponentDocsPreview } from '@/.vitepress/theme/Components/ComponentPreview/Lib/ComponentPreview';
+import type { ComponentDocsPreview } from '@/.vitepress/theme/Components/ComponentDocs/Type/ComponentDocs';
 import type { ApiType }              from '@/Api/Types/Api';
 import type { ComponentName }        from 'flyonui-vue';
 import ComponentPreview              from '@/.vitepress/theme/Components/ComponentPreview/UI/ComponentPreview.vue';
