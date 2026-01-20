e9ffd4cd9c542b0cdf7ba97e5adc8aeb79345857
M.Cozzolino <michaelcozzolino@outlook.it>
2026-01-20 22:30:30 +0100

    release: v2.2.0

--- packages/docs/.vitepress/theme/Components/Layout/Features/Sidebar/Lib/UseSidebarItems.ts
+++ packages/docs/.vitepress/theme/Components/Layout/Features/Sidebar/Lib/UseSidebarItems.ts
@@ -1,6 +1,5 @@
 import type { SidebarBadge, SidebarItem } from '@/.vitepress/theme/Components/Layout/Features/Sidebar/Types/Sidebar';
 import type { ComputedRef }               from 'vue';
-import { flyonUIVueNextPath }             from '@/Next/Lib/Next';
 import { computed }                       from 'vue';
 
 export function useSidebarItems(): ComputedRef<SidebarItem[]> {
@@ -126,7 +125,7 @@ export function useSidebarItems(): ComputedRef<SidebarItem[]> {
                     },
                     {
                         text:     'Tabs',
-                        to:       `${flyonUIVueNextPath}/navigations/tabs`,
+                        to:       '/navigations/tabs',
                         icon:     'mdi:tab',
                         children: [],
                         badge:    _unreleasedBadge,
@@ -185,7 +184,7 @@ export function useSidebarItems(): ComputedRef<SidebarItem[]> {
                     },
                     {
                         text:     'Switch',
-                        to:       `${flyonUIVueNextPath}/forms/switch`,
+                        to:       '/forms/switch',
                         icon:     'mdi:toggle-switch',
                         children: [],
                         badge:    _unreleasedBadge,
