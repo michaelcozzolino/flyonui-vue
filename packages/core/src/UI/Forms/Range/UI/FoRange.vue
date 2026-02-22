3b7e21a21e353b97036ebdb36a7e503eae74fc11
M.Cozzolino <michaelcozzolino@outlook.it>
2026-02-22 21:04:14 +0100

    feat(FoRange): Implement steps slot

--- packages/core/src/UI/Forms/Range/UI/FoRange.vue
+++ packages/core/src/UI/Forms/Range/UI/FoRange.vue
@@ -1,27 +1,50 @@
 <template>
-    <input v-model="input"
-           class="range"
-           :class="[colorClass, sizeClass]"
-           type="range"
-           :min="min"
-           :max="max"
-           :step="step"
-           :disabled="isDisabled"
+    <component :is="slots.steps ? 'div' : FoFragment"
+               :class="slots.steps && 'w-full'"
+               :style="slots.steps && $attrs.style"
     >
+        <input v-model="input"
+               class="range"
+               :class="[colorClass, sizeClass, slots.steps && $attrs.class]"
+               type="range"
+               :min="min"
+               :max="max"
+               :step="step"
+               :disabled="isDisabled"
+               v-bind="slots.steps === undefined && $attrs"
+        >
+
+        <div v-if="slots.steps"
+             class="flex justify-between text-xs px-2"
+        >
+            <slot name="steps" />
+        </div>
+    </component>
 </template>
 
 <script setup lang="ts">
 import type { ComponentName }     from '@/Lib';
 import type { RangeProps }        from '@/UI/Forms/Range';
+import type { Slot }              from 'vue';
 import { useFlyonUIVueAppConfig } from '@/Lib';
 import { useColor }               from '@/Lib/UseColor/Internal';
 import { useSize }                from '@/Lib/UseSize/Internal';
+import { FoFragment }             from '@/UI/Components/Fragment/Internal';
+
+defineOptions({
+    inheritAttrs: false,
+});
 
 const props = withDefaults(defineProps<RangeProps>(), {
     min:  0,
     max:  100,
     step: 1,
 });
+
+const slots = defineSlots<{
+    /** The step indicators to show for each steps */
+    steps?: Slot;
+}>();
 
 const input = defineModel<number>({ required: true });
 
