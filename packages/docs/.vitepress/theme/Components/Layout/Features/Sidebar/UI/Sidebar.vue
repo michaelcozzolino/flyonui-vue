bad75e8bc50d1c31b1f571790902d79dd8097249
M.Cozzolino <michaelcozzolino@outlook.it>
2026-03-20 10:39:07 +0100

    fix(docs): Increase sidebar sticky starting position to avoid it being scrolled under the navbar

--- packages/docs/.vitepress/theme/Components/Layout/Features/Sidebar/UI/Sidebar.vue
+++ packages/docs/.vitepress/theme/Components/Layout/Features/Sidebar/UI/Sidebar.vue
@@ -2,11 +2,12 @@
     <ClientOnly>
         <aside id="flyonui-vue-docs-sidebar"
                ref="sidebar"
-               class="sticky top-16 h-[calc(100vh-4.25rem)] shrink-0 overflow-x-hidden overflow-y-auto"
+               class="sticky shrink-0 overflow-x-hidden overflow-y-auto"
                :class="[
                    isCollapsed ? 'w-24' : 'w-64',
-                   isPageSizeSmallerThanSm && 'fixed! z-1 overflow-x-hidden top-20 transition-[width]',
-                   showMenu && 'bg-base-100'
+                   isPageSizeSmallerThanSm && 'fixed! z-1 overflow-x-hidden transition-[width]',
+                   isPageSizeSmallerThanSm ? 'top-20 h-[calc(100vh-5rem)]' : 'top-18 h-[calc(100vh-4.5rem)]',
+                   showMenu && 'bg-base-100',
                ]"
                tabindex="-1"
         >
