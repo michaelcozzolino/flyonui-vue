3c754721a9dc3de120ede08cbd0a830dbe484531
M.Cozzolino <michaelcozzolino@outlook.it>
2026-03-29 14:15:21 +0200

    feat(FoDataTableHeader): First implementation

--- packages/core/src/UI/Tables/Datatable/Internal/Lib/InjectionKeys.ts
+++ packages/core/src/UI/Tables/Datatable/Internal/Lib/InjectionKeys.ts
@@ -1,6 +1,6 @@
 import type { InjectionKey } from 'vue';
 
-interface DataTableColumnFilterContext<Item extends object = object> {
+export interface DataTableColumnFilterContext<Item extends object = object> {
     /**
      * A function to set the filter that is being used for a specific header
      * @key unique per data table header, where the filter is supposed to be set
--- packages/core/src/UI/Tables/Datatable/UI/FoDataTableHeader.vue
+++ packages/core/src/UI/Tables/Datatable/UI/FoDataTableHeader.vue
@@ -1,41 +1,106 @@
 <template>
-    <FoTableHeader :colspan="colspan" :rowspan="rowspan">
-        <slot />
+    <FoTableHeader :colspan="colspan"
+                   :rowspan="rowspan"
+    >
+        <div class="flex items-center gap-2">
+            <slot />
 
-        <div v-if="filter !== undefined"
-             class="dropdown relative inline-flex"
-        >
-            <FoIcon icon="tabler:filter" />
-
-            <ul class="dropdown-menu dropdown-open:opacity-100 hidden w-auto"
-                role="menu"
-                aria-orientation="vertical"
-                aria-labelledby="dropdown-default"
+            <div v-if="isStringFilter(filter.values)"
+                 class="dropdown relative inline-flex"
             >
-                <li v-for="f in filter[0]"
-                    :key="f"
-                    class="dropdown-item item-name"
-                    @click.prevent="items = items.filter((item): boolean => filter?.[1](item, f) ?? true)"
+                <FoIcon class="cursor-pointer hover:text-warning"
+                        :class="selectedFilterValue !== null && 'text-warning'"
+                        icon="tabler:filter"
+                        size="doubleExtraSmall"
+                        @click.prevent="showFilterValues = !showFilterValues"
+                />
+
+                <ul v-show="showFilterValues"
+                    ref="dropdown"
+                    class="dropdown-menu absolute inset-e-0 top-full z-10 mt-2 w-auto opacity-100"
+                    role="menu"
+                    aria-orientation="vertical"
                 >
-                    {{ f }}
-                </li>
-            </ul>
+                    <li v-for="value in filter.values"
+                        :key="value"
+                        class="cursor-pointer dropdown-item item-name"
+                        :class="selectedFilterValue === value && 'text-warning'"
+                        @click.prevent="onFilterClick(value)"
+                    >
+                        {{ value }}
+                    </li>
+                </ul>
+            </div>
         </div>
     </FoTableHeader>
 </template>
 
-<script setup lang="ts" generic="Item extends object, Filter extends string | number">
-import type { WithDefaultSlot } from '@/Types';
-import type { TableCellProps }  from '@/UI/Tables';
-import { FoIcon }               from '@/UI/Customization';
-import { FoTableHeader  }       from '@/UI/Tables';
+<script setup lang="ts" generic="Item extends object, Value extends string | number">
+import type { WithDefaultSlot }              from '@/Types';
+import type { DataTableHeaderProps }         from '@/UI/Tables';
+import type { DataTableColumnFilterContext } from '@/UI/Tables/Datatable/Internal/Lib';
+import { useSafeInjection }                  from '@/Lib/UseSafeInjection/Internal';
+import { FoIcon }                            from '@/UI/Customization';
+import { FoTableHeader }                     from '@/UI/Tables';
+import {
 
-interface DatatableHeaderProps<Item extends object, Filter extends string | number> extends TableCellProps {
-    filter?: [Filter[], (item: Item, filter: Filter) => boolean];
-}
-const props = defineProps<DatatableHeaderProps<Item, Filter>>();
+    dataTableColumnFilterInjectionKey,
+} from '@/UI/Tables/Datatable/Internal/Lib';
+import { onClickOutside }             from '@vueuse/core';
+import { ref, useTemplateRef, watch } from 'vue';
+
+const props = defineProps<DataTableHeaderProps<Item, Value>>();
 
 defineSlots<WithDefaultSlot>();
 
-const items        = defineModel<Item[]>('items', { default: [] });
+const filterKey       = Symbol('dataTableHeaderFilter');
+const dataTableFilter = useSafeInjection<DataTableColumnFilterContext<Item>>(dataTableColumnFilterInjectionKey);
+
+const selectedFilterValue = ref<Value | null>(null);
+const showFilterValues    = ref<boolean>(false);
+
+const dropdownElement = useTemplateRef('dropdown');
+
+onClickOutside(dropdownElement, (): void => {
+    showFilterValues.value = false;
+});
+
+watch(selectedFilterValue, () => {
+    if (selectedFilterValue.value === null) {
+        dataTableFilter.setFilter(filterKey, null);
+
+        return;
+    }
+
+    dataTableFilter.setFilter(filterKey, (item: Item): boolean => {
+        return props.filter.onFilter(item, selectedFilterValue.value);
+    });
+}, { immediate: true });
+
+function isStringFilter<T extends Value>(filterValues: T[]): filterValues is (T & string)[] {
+    for (const value of filterValues) {
+        if (typeof value !== 'string') {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function onFilterClick(value: Value & string) {
+    if (selectedFilterValue.value === value) {
+        selectedFilterValue.value = null;
+        showFilterValues.value = false;
+
+        return;
+    }
+
+    selectedFilterValue.value = value;
+
+    dataTableFilter.setFilter(filterKey, (item: object): boolean => {
+        return props.filter.onFilter(item as Item, value);
+    });
+
+    showFilterValues.value = false;
+}
 </script>
