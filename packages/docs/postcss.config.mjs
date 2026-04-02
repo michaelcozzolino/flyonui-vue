ad7f5e9ce84f67edc2fcd12ffa3c77dc8470936e
M.Cozzolino <michaelcozzolino@outlook.it>
2026-04-02 11:26:39 +0200

    chore(docs): Use tailwindcss plugin instead of postcss

--- packages/docs/postcss.config.mjs
+++ packages/docs/postcss.config.mjs
@@ -1,9 +1,7 @@
-import tailwindcss              from '@tailwindcss/postcss';
 import { postcssIsolateStyles } from 'vitepress';
 
 export default {
     plugins: [
         postcssIsolateStyles(),
-        tailwindcss(),
     ],
 };
